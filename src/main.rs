c4c2d776a292e585f718c1d89854333113fdcc96
Aitor Fernandez <helloimaitor@gmail.com>
2022-04-27 18:38:36 +0100

    string return and naming

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use rand::distributions::Alphanumeric;
-use rand::{thread_rng, Rng};
-use std::process;
-use std::sync::atomic::{AtomicU8, Ordering};
-use std::time::{SystemTime, UNIX_EPOCH};
+use rand::{distributions::Alphanumeric, thread_rng, Rng};
+use std::{
+    sync::atomic::{AtomicU8, Ordering},
+    time::{SystemTime, UNIX_EPOCH},
+};
 
 static BASE_36: u8 = 36;
 static COUNTER: AtomicU8 = AtomicU8::new(0);
@@ -16,7 +16,7 @@ fn to_base36(mut v: u128) -> String {
     chars.into_iter().rev().collect()
 }
 
-fn rnd(elements: usize) -> String {
+fn rnd_string(elements: usize) -> String {
     thread_rng()
         .sample_iter(&Alphanumeric)
         .take(elements)
@@ -53,21 +53,21 @@ pub fn puid(pref: &str, elements: usize) -> String {
         "Prefix cannot be longer than 3 characters and non-alphanumeric characters"
     );
 
-    // pref + 24 bytes = time 8 + counter 3 + process 4 + rand default 9
-    format!(
-        "{}_{}_{:0>3}_{}_{}",
+    [
         pref,
-        to_base36(time()),
-        counter(),
-        to_base36(process::id() as u128),
-        rnd(elements)
-    )
+        "_",
+        &to_base36(time()),
+        &counter().to_string(),
+        &to_base36(std::process::id() as u128).to_string(),
+        &rnd_string(elements),
+    ]
+    .concat()
 }
 
 #[macro_export]
 macro_rules! puid {
     ($pref:expr) => {
-        $crate::puid($pref, 9)
+        $crate::puid($pref, 12)
     };
 
     ($pref:expr, $elements:expr) => {
@@ -76,9 +76,7 @@ macro_rules! puid {
 }
 
 fn main() {
-    for _ in 0..10 {
-        println!("> {}", puid!("pre"));
-    }
+    println!("> {}", puid!("pre"));
 }
 
 #[cfg(test)]
@@ -87,7 +85,7 @@ mod tests {
     use std::collections::HashMap;
 
     #[test]
-    fn prefix_test() {
+    fn validate_test() {
         let tests = HashMap::from([
             ("f", true),
             ("fo", true),
@@ -100,5 +98,10 @@ mod tests {
         for (pref, res) in tests {
             assert_eq!(validate(pref), res);
         }
+    }
+
+    #[test]
+    fn rnd_string_test() {
+        assert_eq!(rnd_string(12).len(), 12);
     }
 }
